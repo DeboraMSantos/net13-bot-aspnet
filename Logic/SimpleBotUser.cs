e3ed7f0aa5b25a6acefd03c16c297382232153ec
Debora <debora.moura.santos@outlook.com.br>
2018-09-05 22:38:37 -0300

    [Aula] - Mongo

--- Logic/SimpleBotUser.cs
+++ Logic/SimpleBotUser.cs
@@ -10,36 +10,80 @@ namespace SimpleBot
 {
     public class SimpleBotUser
     {
+        static Dictionary<string, UserProfile> _dictProfiles = new Dictionary<string, UserProfile>();
         public static string Reply(Message message)
         {
-            MongoClient client = new MongoClient("mongodb://localhost:27017");
-            IMongoDatabase db = client.GetDatabase("db01");
-            IMongoCollection<BsonDocument> col = db.GetCollection<BsonDocument>("tabela01");
-            BsonDocument doc = new BsonDocument()
-            {
-                {"id", message.Id},
-                {"texto", message.Text},
-                {"usuario", message.User},
-                {"app", "testeBot"}
-            };
+            //    MongoClient client = new MongoClient("mongodb://localhost:27017");
+            //    IMongoDatabase db = client.GetDatabase("db01");
+            //    IMongoCollection<BsonDocument> col = db.GetCollection<BsonDocument>("tabela01");
+            //    BsonDocument doc = new BsonDocument()
+            //    {
+            //        {"id", message.Id},
+            //        {"texto", message.Text},
+            //        {"usuario", message.User},
+            //        {"app", "testeBot"}
+            //    };
 
-            col.InsertOne(doc);
+            //    col.InsertOne(doc);
 
-            return $"{message.User} disse:'{message.Text}'";
+            //    UserProfile getProfile = GetProfile(message.Id);
+            //    SetProfile(message.Id, getProfile);
+
+            //    return $"{message.Id} - {getProfile.Visitas}";
+            var id = message.Id;
+            var profile = GetProfile(id);
+            profile.Visitas++;
+            SetProfile(id, profile);
+            return $"{message.User} disse '{message.Text}' e mandou {profile.Visitas}";
         }
 
         public static UserProfile GetProfile(string id)
         {
-            MongoClient client = new MongoClient("mongodb://localhost:27017");
-            IMongoDatabase db = client.GetDatabase("db01");
-            IMongoCollection<BsonDocument> col = db.GetCollection<BsonDocument>("tabela01");
-            var filter = Builders<BsonDocument>.Filter.Eq("usuario", "User");
-            var a = col.Find(filter).ToList();
-          //  return a;
+            //MongoClient client = new MongoClient("mongodb://localhost:27017");
+            //IMongoDatabase db = client.GetDatabase("db01");
+            //IMongoCollection<BsonDocument> col = db.GetCollection<BsonDocument>("tabela01");
+            //var filter = Builders<BsonDocument>.Filter.Eq("id", id);
+            //var a = col.Find(filter).ToList();
+
+            //UserProfile userProfile = new UserProfile()
+            //{
+            //    Id = id,
+            //    Visitas = a.Count()
+            //};             
+
+            //return userProfile;
+
+            _dictProfiles.TryGetValue(id, out var profile);
+
+            if(profile == null)
+            {
+
+            }
         }
+        public static void SalvarHistorico(Message message) { }
 
         public static void SetProfile(string id, UserProfile profile)
         {
+            MongoClient client = new MongoClient("mongodb://localhost:27017");
+            IMongoDatabase db = client.GetDatabase("db01");
+            IMongoCollection<BsonDocument> col = db.GetCollection<BsonDocument>("profile");
+            var filter = Builders<BsonDocument>.Filter.Eq("id", id);
+            BsonDocument doc = new BsonDocument()
+            {
+                {"id", id},
+                {"visitas",profile.Visitas},
+                {"app", "testeBot"}
+            };
+
+            if (profile.Visitas > 100)
+            {
+                col.UpdateOne(filter,doc);
+            }
+            else
+            {
+                col.InsertOne(doc);
+            }
+
         }
     }
 }
